f0d855c87bf42a88289b1f6f2ed528c40bc4e00f
inser788 <inser7@abv.bg>
2018-01-21 19:57:37 +0200

    add exercises

---
+++ Exercises/06 MATRICES-Exercises/Solution1/01 Matrix of Palindromes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Matrix_of_Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rowsCount = matrixSize[0];
+            int colsCount = matrixSize[1];
+            string[][] matrix=new string[rowsCount][];
+            for (int row = 0; row < rowsCount; row++)
+            {
+                matrix[row]=new string[colsCount];
+
+                for (int col = 0; col < colsCount; col++)
+                {
+                    StringBuilder builderPalindrome=new StringBuilder();
+                    char firstLetter = (char) ('a' + row);
+                    char secondLetter = (char) (firstLetter + col);
+                    builderPalindrome
+                        .Append(firstLetter)
+                        .Append(secondLetter)
+                        .Append(firstLetter);
+                    matrix[row][col] = builderPalindrome.ToString();
+                }
+            }
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ",row));
+            }
+        }
+    }
+}
---
+++ Exercises/06 MATRICES-Exercises/Solution1/02 Diagonal Difference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[sizeMatrix][];
+            for (int row = 0; row < sizeMatrix; row++)
+            {
+                matrix[row] = new int[sizeMatrix];
+                int[] numbers = Console.ReadLine()
+                    .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < sizeMatrix; col++)
+                {
+                    matrix[row][col] = numbers[col];
+                }
+            }
+           
+            int sumFirst = 0;
+            int sumLast = 0;
+            int indexFirst = 0;
+            int indexLast = sizeMatrix - 1;
+            foreach (var row in matrix)
+            {
+                sumFirst += row.ElementAt(indexFirst);
+                sumLast += row.ElementAt(indexLast);
+                indexFirst++;
+                indexLast--;
+            }
+
+            int absoluteDeviation = Math.Abs(sumFirst - sumLast);
+            Console.WriteLine(absoluteDeviation);
+        }
+    }
+}
