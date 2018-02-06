a31c8ee8b31dfa631aa1bc8c13dc617de9420e22
inser788 <inser7@abv.bg>
2018-02-06 20:42:46 +0200

    new info

--- Exams/20170625/Solution1/00 test/Program.cs
+++ Exams/20170625/Solution1/00 test/Program.cs
@@ -1,14 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Linq;
-
-namespace FindEvensOrOdds
+using System.Collections.Generic;
+namespace _04.Regeh
 {
-    class Program
+    class Regeh
     {
-        static void Main()
+        static void Main(string[] args)
         {
-           
+            var input = Console.ReadLine();
+            var pattern = @"\[[A-Z&a-z]+<([0-9]+)REGEH([0-9]+)>[A-Z&a-z]+\]";
+            var regex = Regex.Matches(input, pattern);
+            var nums = new List<int>();
+
+            foreach (Match match in regex)
+            {
+                nums.Add(int.Parse(match.Groups[1].Value));
+                nums.Add(int.Parse(match.Groups[2].Value));
+            }
+
+            int numSum = 0;
+            string result = string.Empty;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                numSum += nums[i];
+                numSum %= input.Length;
+                result += input[numSum];
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
