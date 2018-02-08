fa6557d268e4d256640a8e8a49dfaee6efd864d5
inser788 <inser7@abv.bg>
2018-02-09 00:03:33 +0200

    new exercises

---
+++ Exams/20160619/Solution1/00 test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_CubicAssault
+{
+    public class CubicAssault
+    {
+
+        public static void Main()
+        {
+            int n1 = -1000000 / 1000000;
+            int n2 = -1000005 % 1000000;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+
+    }
+}
---
+++ Exams/20160619/Solution1/03 Cubic Messages/CubicMessage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03_Cubic_Messages
+{
+    class CubicMessage
+    {
+        static void Main()
+        {
+            string pattern = @"^([\d]+)([a-zA-Z]+)([^a-zA-Z]*?)$";
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input=="Over!")
+                    break;
+
+                int lengthOfWord = int.Parse(Console.ReadLine());
+
+                var match = Regex.Match(input, pattern);
+                if (!match.Success&&match.Groups[2].Length!=lengthOfWord)
+                {
+                    continue;
+                }
+
+                string digitsBeforeWord = match.Groups[1].ToString();
+                var listDigitsBefore = digitsBeforeWord
+                    .ToCharArray()
+                    .Select(x => int.Parse(x.ToString()))
+                    .ToList();
+                string word = match.Groups[2].ToString();
+                char[] allSymbolsAfterWord = match.Groups[3].ToString().ToCharArray();
+                var listOfDigitsAfter = allSymbolsAfterWord
+                    .Where(a => char.IsDigit(a))
+                    .Select(x => int.Parse(x.ToString()))
+                    .ToList();
+               var verificatonCode = new StringBuilder();
+                for (int i = 0; i < listDigitsBefore.Count; i++)
+                {
+                    int currentElement = listDigitsBefore[i];
+                    if (currentElement>word.Length-1)
+                    {
+                        verificatonCode.Append(" ");
+                        continue;
+                    }
+
+                    verificatonCode.Append(word[currentElement]) ;
+                }
+
+                for (int i = 0; i < listOfDigitsAfter.Count; i++)
+                {
+                    int currentElement = listOfDigitsAfter[i];
+                    if (currentElement > word.Length - 1)
+                    {
+                        verificatonCode.Append(" ");
+                        continue;
+                    }
+
+                    verificatonCode.Append(word[currentElement]);
+                }
+                Console.WriteLine($"{word} == {verificatonCode}");
+            }
+        }
+    }
+}
