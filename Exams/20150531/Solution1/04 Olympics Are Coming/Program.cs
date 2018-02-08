f6fcd0d24088b366f6d8cd48b7076b05d5a22b60
inser788 <inser7@abv.bg>
2018-02-08 19:31:07 +0200

    new exercises

--- Exams/20150531/Solution1/04 Olympics Are Coming/Program.cs
+++ Exams/20150531/Solution1/04 Olympics Are Coming/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _04_Olympics_Are_Coming
@@ -11,39 +12,41 @@ namespace _04_Olympics_Are_Coming
         static void Main(string[] args)
         {
             var dataForCountries = new Dictionary<string, Dictionary<string, int>>();
-
+            string pattern = @"\s*\|\s*";
             while (true)
             {
-                string input = Console.ReadLine().Trim();
+                string input = Console.ReadLine();
                 if (input == "report")
                     break;
-                string[] dataInput = input.Trim().Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-                string athletName = dataInput.First().Trim();
-                string athletNameOriginal = string.Join(" ", athletName.Split(" \t\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries));
-                athletNameOriginal = athletNameOriginal.Trim();
-                string countryName = dataInput.Last().Trim();
+
+                var dataInput = Regex.Split(input, pattern);
+
+                var athletNameList = dataInput.First().Split(" \t".ToCharArray()).Where(s => !string.IsNullOrWhiteSpace(s));
+                string athletName = string.Join(" ", athletNameList);
+                var countryNameList = dataInput.Last().Split(" \t".ToCharArray()).Where(s => !string.IsNullOrWhiteSpace(s));
+                string countryName = string.Join(" ", countryNameList);
                 if (!dataForCountries.ContainsKey(countryName))
                 {
                     dataForCountries[countryName] = new Dictionary<string, int>();
-                    dataForCountries[countryName].Add(athletNameOriginal, 1);
+                    dataForCountries[countryName].Add(athletName, 1);
+                    continue;
                 }
-                else
+
+                if (!dataForCountries[countryName].ContainsKey(athletName))
                 {
-                    if (!dataForCountries[countryName].ContainsKey(athletNameOriginal))
-                    {
-                        dataForCountries[countryName][athletNameOriginal] = 1;
-                    }
-                    else
-                    {
-                        dataForCountries[countryName][athletNameOriginal] ++;
-                    }
+                    dataForCountries[countryName][athletName] = 1;
+                    continue;
                 }
+
+                dataForCountries[countryName][athletName]++;
             }
+
             var orderedCountries = dataForCountries.OrderByDescending(x => x.Value.Values.Sum())
                 .ToDictionary(k => k.Key, v => v.Value);
             foreach (var item in orderedCountries)
             {
-                Console.WriteLine($"{item.Key} ({item.Value.Keys.Count()} participants): {item.Value.Values.Sum()} wins");
+                Console.WriteLine(
+                    $"{item.Key} ({item.Value.Keys.Count()} participants): {item.Value.Values.Sum()} wins");
             }
         }
     }
--- Exams/20170903/Solution1/03 Greedy Times/GreedyTimes.cs
+++ Exams/20170903/Solution1/03 Greedy Times/GreedyTimes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,9 @@ namespace _03_Greedy_Times
         {
             long goldCapacity = 0;
             var gemsCapacity = new Dictionary<string, long>();
+            var gemsNamesUpper=new List<string>();
             var cashCapacity = new Dictionary<string, long>();
+            var cashNamesUpper=new List<string>();
 
             long maxCashValue = long.Parse(Console.ReadLine());
             long allSumInbag = 0;
@@ -37,16 +40,11 @@ namespace _03_Greedy_Times
                 foreach (var item in valuableItemsNames.Zip(valuableItemsQuantities, Tuple.Create))
                 {
                     counter++;
-                    bool isItemUseful = item.Item1 == "Gold" ||
-                                        (item.Item1.EndsWith("gem") && item.Item1.Length >= 4) ||
-                                        (item.Item1.Length == 3 && item.Item1.All(a => char.IsLetter(a)));
-                    if (!isItemUseful)
-                    {
-                        continue;
-                    }
+                    
 
                     var key = item.Item1;
-                    if (key == "Gold" && goldCapacity >= gemsCapacity.Values.Sum())
+                    var upperName = item.Item1.ToUpperInvariant();
+                    if (upperName == "GOLD" && goldCapacity >= gemsCapacity.Values.Sum())
                     {
                         if ((allSumInbag + item.Item2) <= maxCashValue)
                         {
@@ -56,13 +54,14 @@ namespace _03_Greedy_Times
                     }
 
 
-                    else if (key.EndsWith("gem") && key.Length >= 4 )
+                    else if (upperName.EndsWith("GEM") && key.Length > 3 )
                     {
                         if ((allSumInbag + item.Item2) <= maxCashValue && goldCapacity >= gemsCapacity.Values.Sum()+ item.Item2)
                         {
-                            if (!gemsCapacity.ContainsKey(key))
+                            if (!gemsCapacity.ContainsKey(key)&&!gemsNamesUpper.Contains(upperName))
                             {
                                 gemsCapacity[key] = 0;
+                                gemsNamesUpper.Add(upperName);
                             }
 
                             gemsCapacity[key] += item.Item2;
@@ -70,13 +69,14 @@ namespace _03_Greedy_Times
                         }
                     }
 
-                    else if (key.Length == 3)
+                    else if (key.Length == 3&&key.All(a=>char.IsLetter(a)))
                     {
                         if ((allSumInbag + item.Item2) <= maxCashValue && gemsCapacity.Values.Sum() >= cashCapacity.Values.Sum()+ item.Item2)
                         {
-                            if (!cashCapacity.ContainsKey(key))
+                            if (!cashCapacity.ContainsKey(key)&&!cashNamesUpper.Contains(upperName))
                             {
                                 cashCapacity[key] = 0;
+                                cashNamesUpper.Add(upperName);
                             }
 
                             cashCapacity[key] += item.Item2;
