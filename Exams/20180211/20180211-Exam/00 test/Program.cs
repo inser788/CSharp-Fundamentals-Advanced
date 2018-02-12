8ffbc3c4150ad174686ef593aa244c3cf15d0dfc
inser788 <inser7@abv.bg>
2018-02-12 19:51:11 +0200

    add exam 20180211-Exam and solutions

---
+++ Exams/20180211/20180211-Exam/00 test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                builder.Append(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
